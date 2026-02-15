98cf4ba6dc44afceb779fa954f177676de7e77b6
Furret78 <68061718+furret78@users.noreply.github.com>
2026-02-15 02:28:17 +0100

    map complete (except challenge)

--- scripts/items_import.lua
+++ scripts/items_import.lua
@@ -1,4 +1,6 @@
 
-Tracker:AddItems("items/items.json")
-Tracker:AddItems("items/labels.json")
-                
+Tracker:AddItems("items/items.json") -- Ability Cards here.
+Tracker:AddItems("items/stage_unlock.json") -- Stage unlock display.
+Tracker:AddItems("items/boss_defeat.json") -- Boss defeat display.
+Tracker:AddItems("items/update.json") -- Updates. Don't change anything here.
+Tracker:AddItems("items/filler.json") -- Useful, Filler, and Traps.
